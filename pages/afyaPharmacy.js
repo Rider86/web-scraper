dded0eb80bd214d3ca71dbb0788670652e77dcba
deian_ivanov <deian_ivanov2003@yahoo.com>
2023-08-10 00:15:56 +0300

    fix variables names again

--- pages/afyaPharmacy.js
+++ pages/afyaPharmacy.js
@@ -7,10 +7,21 @@ import Pharmacy from '../components/Pharmacy';
 import Navbar from '../components/Navbar';
 
 
- 
+export const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+
  export const getServerSideProps = async () => {
-    const auth = await google.auth.getClient({ scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'] });
-  
+  const { privateKey } = JSON.parse(process.env.GOOGLE_PRIVATE_KEY || { privateKey: null })
+  const auth = new google.auth.GoogleAuth({
+    scopes: SCOPES,
+    projectId: process.env.GOOGLE_PROJECTID,
+    credentials: {
+      private_key: privateKey,
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    },
+   
+  }
+ 
+  )  
     const sheets = google.sheets({ version: 'v4', auth });
    
     const range = `afya!A1:A1000`;
